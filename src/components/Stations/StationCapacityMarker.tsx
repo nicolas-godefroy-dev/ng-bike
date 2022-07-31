3b3fe79fb4bc3326a44d9c9eb3b5665f77502a8d
Nicolas Godefroy <nicolas.godefroy.dev@gmail.com>
2022-07-31 21:12:46 +0200

    chore: refresh markers UI

--- src/components/Stations/StationCapacityMarker.tsx
+++ src/components/Stations/StationCapacityMarker.tsx
@@ -14,20 +14,38 @@ const StationCapacityMarker = memo(
     const { colors, shadows } = useTheme()
 
     return (
-      <View
-        style={[
-          styles.container,
-          { backgroundColor: colors.surface.base },
-          shadows["md"],
-        ]}>
-        <StationCapacity {...stationCapacityProps} />
+      <View style={styles.container}>
+        <View
+          style={[
+            styles.content,
+            { backgroundColor: colors.surface.base },
+            shadows["md"],
+          ]}>
+          <StationCapacity {...stationCapacityProps} />
+        </View>
+        <View
+          style={[styles.triangle, { backgroundColor: colors.surface.base }]}
+        />
       </View>
     )
   },
 )
 
 const styles = StyleSheet.create({
+  triangle: {
+    marginTop: -6,
+    borderRadius: borderRadius["sm"],
+    height: spacing[2.5],
+    width: spacing[2.5],
+    transform: [{ rotateZ: "45deg" }],
+  },
   container: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: spacing[12],
+    marginBottom: spacing[12],
+  },
+  content: {
     borderRadius: borderRadius["3xl"],
     alignItems: "center",
     justifyContent: "center",
