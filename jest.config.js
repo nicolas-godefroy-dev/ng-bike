6f3d338ff8431288da06b989c7e81fd9db710e91
Nicolas Godefroy <nicolas.godefroy.dev@gmail.com>
2022-07-31 15:02:33 +0200

    chore: implement test coverage

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  preset: "jest-expo",
+  transformIgnorePatterns: [
+    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
+  ],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "**/*.{ts,tsx}",
+    "!**/coverage/**",
+    "!**/node_modules/**",
+  ],
+  coverageThreshold: {
+    global: {
+      lines: 5,
+    },
+  },
+}
