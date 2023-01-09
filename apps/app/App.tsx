49e13ecf99b234b3beefdc994f695c6d1197147e
Nicolas Godefroy <nicolas.godefroy.dev@gmail.com>
2023-01-09 07:34:43 +0100

    refactor: migrate to a monorepo

--- apps/app/App.tsx
+++ apps/app/App.tsx
@@ -4,13 +4,13 @@ import { StyleSheet } from "react-native"
 import { GestureHandlerRootView } from "react-native-gesture-handler"
 import { SafeAreaProvider } from "react-native-safe-area-context"
 import { QueryClientProvider } from "react-query"
-import { useColorScheme } from "src/theme/hooks/useColorScheme"
 
 import { AppLoading } from "@components/AppLoading"
 import { useCachedResources } from "@hooks/useCachedResources"
 import { useRefetchOnAppFocus } from "@hooks/useRefetchOnAppFocus"
 import queryClient from "@libs/queryClient"
 import Navigation from "@navigation/Navigation"
+import { useColorScheme } from "@theme"
 
 export default function App() {
   const isLoadingComplete = useCachedResources()
