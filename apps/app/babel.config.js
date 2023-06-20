41a187bf6787e15602e2ea0799d844e193773a2e
Nicolas Godefroy <nicolas.godefroy.dev@gmail.com>
2023-06-20 10:25:52 +0400

    fix: quality assurance workflow

--- apps/app/babel.config.js
+++ apps/app/babel.config.js
@@ -2,6 +2,10 @@ const path = require('path');
 
 const config = require('@ng-bike/babel-config-react-native');
 
+/**
+ * Custom config for expo router
+ * @see https://github.com/expo/router/issues/41
+ **/
 process.env.EXPO_ROUTER_APP_ROOT = path.resolve(__dirname, '/src/routes');
 
 module.exports = function (api) {
--- apps/app/index.jsx
+++ apps/app/index.jsx
@@ -4,6 +4,10 @@ import React from 'react';
 
 // Must be exported or Fast Refresh won't update the context
 export function App() {
+  /**
+   * Custom config for expo router
+   * @see https://github.com/expo/router/issues/41
+   **/
   const ctx = require.context('./src/routes');
 
   return <ExpoRoot context={ctx} />;
