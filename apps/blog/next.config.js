3f92bfbab745de41962b54c5e82577efc91532be
Nicolas Godefroy <nicolas.godefroy.dev@gmail.com>
2023-07-04 18:16:56 +0200

    fix(blog): next config

--- apps/blog/next.config.js
+++ apps/blog/next.config.js
@@ -1,20 +1,7 @@
-const prismic = require('@prismicio/client');
-
-const sm = require('./slicemachine.config.json');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = async () => {
-  const client = prismic.createClient(sm.repositoryName);
-
-  const repository = await client.getRepository();
-  const locales = repository.languages.map((lang) => lang.id);
-
   return {
     reactStrictMode: true,
-    i18n: {
-      locales,
-      defaultLocale: locales[0],
-    },
   };
 };
 
