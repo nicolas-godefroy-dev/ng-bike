95ee8867f3ad984a11e2c585cfd572bd1ac23b70
Nicolas Godefroy <nicolas.godefroy.dev@gmail.com>
2023-07-06 11:53:18 +0200

    fix(blog): prismic production

--- apps/blog/src/libs/prismic/client.ts
+++ apps/blog/src/libs/prismic/client.ts
@@ -39,10 +39,7 @@ const routes: prismic.ClientConfig['routes'] = [
 export const createClient = (config: prismicNext.CreateClientConfig = {}) => {
   const client = prismic.createClient(repositoryName, {
     routes,
-    fetchOptions:
-      process.env.NODE_ENV === 'production'
-        ? { next: { tags: ['prismic'] }, cache: 'force-cache' }
-        : { next: { revalidate: 5 } },
+    fetchOptions: { next: { revalidate: 5 } },
     ...config,
   });
 
